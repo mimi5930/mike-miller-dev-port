ac95bfab15a1ad40453806decba6dcc9430e6867
mimi5930 <mikej.miller440@gmail.com>
2022-05-16 08:35:27 -0500

    social icons

--- client/src/components/Socials/index.js
+++ client/src/components/Socials/index.js
@@ -1,9 +1,36 @@
-import React, { useState } from 'react';
-import { Button, Flex, Heading } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import { IconButton, Button, Flex, Heading, Text } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
+import { FiGithub, FiLinkedin } from 'react-icons/fi';
 
 const Socials = props => {
-  return <header></header>;
+  const [currentIcon, setIcon] = useState(0);
+
+  const icons = [<FiGithub />, <FiLinkedin />];
+
+  useEffect(() => {
+    let count = 0;
+
+    function cycleArray() {
+      setIcon(count);
+      count++;
+      if (count === icons.length) count = 0;
+    }
+
+    setInterval(cycleArray, 5000);
+  }, []);
+
+  return (
+    <footer style={{ position: 'fixed', bottom: '0', right: '0' }}>
+      <IconButton
+        borderRadius="50%"
+        m="8"
+        colorScheme="blackAlpha"
+        size="lg"
+        icon={icons[currentIcon]}
+      ></IconButton>
+    </footer>
+  );
 };
 
 export default Socials;

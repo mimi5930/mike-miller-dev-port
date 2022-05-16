04e11eb165c81943af94367017b2c8803c8a04ef
mimi5930 <mikej.miller440@gmail.com>
2022-05-15 20:06:17 -0500

    added logo

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import { Flex, Button, Link } from '@chakra-ui/react';
+import { Flex, Button, Link, Image } from '@chakra-ui/react';
 
 const Header = props => {
   return (
     <header>
       <Flex
-        justifyContent="flex-end"
+        justifyContent="space-between"
         alignItems="center"
         p="15"
-        backgroundColor="gray.400"
+        backgroundColor="black"
+        color="white"
       >
-        <Link mr="15">About</Link>
-        <Link mr="15">Projects</Link>
-        <Link mr="15">Contact</Link>
-        <Button colorScheme="blue">Resume</Button>
+        <Image
+          boxSize="90px"
+          src={require('../../img/mike-logo.png')}
+          alt="Website logo"
+        />
+        <Flex justifyContent="space-evenly" alignItems="center">
+          <Link mr="15">About</Link>
+          <Link mr="15">Projects</Link>
+          <Link mr="15">Resume</Link>
+          <Button variant="outline" colorScheme="white">
+            Contact
+          </Button>
+        </Flex>
       </Flex>
     </header>
   );

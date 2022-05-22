dab69a14d88038f834ed30e220a32892543d6cb9
Mike <mikej.miller440@gmail.com>
2022-05-22 17:44:13 -0500

    project styling

--- client/src/components/Portfolio/index.js
+++ client/src/components/Portfolio/index.js
@@ -1,44 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import projects from './featProjects.json';
-import { Box, Center, Heading, Image, Text, LinkBox } from '@chakra-ui/react';
+import {
+	Box,
+	Center,
+	Heading,
+	Image,
+	Text,
+	LinkBox,
+	useDisclosure,
+	Collapse
+} from '@chakra-ui/react';
 
 const Portfolio = props => {
-  const projectTest = {
-    name: 'Ritual',
-    pic: 'ritual.png',
-    alt: "ritual's homepage",
-    features: 'MERN, GraphQL, Chakra UI, React useContext',
-    url: 'https://ritual-app-01.herokuapp.com/',
-    gitHub: 'https://github.com/Metelak/Ritual'
-  };
+	const { isOpen, onToggle } = useDisclosure();
 
-  return (
-    <>
-      <Center>
-        <Heading color="white" mt="8rem">
-          Featured Work
-        </Heading>
-      </Center>
-      <Box maxW="md" overflow="hidden" borderRadius="md" bgColor="white">
-        <Image
-          src={require(`../../img/projects/${projectTest.pic}`)}
-          alt={projectTest.alt}
-        />
-        <Box bgColor="white">
-          <Center>
-            <Heading m="2">{projectTest.name}</Heading>
-          </Center>
-          <Center>
-            <Text m="2" mb="5">
-              {projectTest.features}
-            </Text>
-          </Center>
-        </Box>
-      </Box>
+	const projectTest = projects[0];
 
-      <div>Projects</div>
-    </>
-  );
+	return (
+		<>
+			<Center>
+				<Heading color="white" m="5" mt="8rem">
+					Featured Work
+				</Heading>
+			</Center>
+			<Box
+				onMouseEnter={onToggle}
+				onMouseLeave={onToggle}
+				width="300px"
+				overflow="hidden"
+				borderRadius="md"
+				bgColor="white"
+			>
+				<Image
+					src={require(`../../img/projects/${projectTest.pic}`)}
+					alt={projectTest.alt}
+				/>
+				<Collapse in={isOpen}>
+					<Box bgColor="white">
+						<Heading m="2">{projectTest.name}</Heading>
+
+						<Text m="2" mb="5">
+							{projectTest.features}
+						</Text>
+					</Box>
+				</Collapse>
+			</Box>
+
+			<div>Projects</div>
+		</>
+	);
 };
 
 export default Portfolio;

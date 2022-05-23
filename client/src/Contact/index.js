0deec701cbf321e9e4b7e3c9a0d7b32a331dc711
Mike <mikej.miller440@gmail.com>
2022-05-23 11:13:25 -0500

    added fields to modal

--- client/src/Contact/index.js
+++ client/src/Contact/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import {
   Button,
   Modal,
@@ -7,23 +7,50 @@ import {
   ModalHeader,
   ModalCloseButton,
   ModalBody,
-  ModalFooter
+  ModalFooter,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea
 } from '@chakra-ui/react';
 
 const Contact = ({ isOpen, onClose }) => {
+  const initialRef = useRef();
+
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal isOpen={isOpen} onClose={onClose} initialFocusRef={initialRef}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Modal Title</ModalHeader>
+        <ModalHeader>
+          Thank you for visiting! Please leave a message with any questions.
+        </ModalHeader>
         <ModalCloseButton />
-        <ModalBody></ModalBody>
+        <ModalBody>
+          <FormControl pb="6">
+            <FormLabel>Name</FormLabel>
+            <Input ref={initialRef} />
+          </FormControl>
+          <FormControl pb="6">
+            <FormLabel>Email *</FormLabel>
+            <Input />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Message *</FormLabel>
+            <Textarea resize="none" />
+          </FormControl>
+        </ModalBody>
 
         <ModalFooter>
-          <Button colorScheme="blue" mr={3} onClick={onClose}>
-            Close
+          <Button colorScheme="purple" mr={3}>
+            Submit
           </Button>
-          <Button variant="ghost">Secondary Action</Button>
+          <Button onClick={onClose}>Cancel</Button>
         </ModalFooter>
       </ModalContent>
     </Modal>

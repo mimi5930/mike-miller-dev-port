958ccd9684a37da5ce7c23fa6e6f7d9876cbc3cf
Mike <mikej.miller440@gmail.com>
2022-05-30 19:13:27 -0500

    restyled about

--- client/src/pages/About/index.js
+++ client/src/pages/About/index.js
@@ -3,46 +3,48 @@ import React from 'react';
 
 const About = props => {
   return (
-    <section>
+    <Flex
+      mt="20vh"
+      bgColor="blackAlpha.900"
+      width="80%"
+      mr="auto"
+      ml="auto"
+      height="60vh"
+      borderRadius="lg"
+      alignItems="center"
+      justifyContent="space-evenly"
+      flexDir="column"
+    >
+      <Heading size="4xl" color="whiteAlpha.800">
+        Mike Miller
+      </Heading>
       <Flex
-        mt="20vh"
-        bgColor="blackAlpha.900"
-        width="80%"
-        mr="auto"
-        ml="auto"
-        height="70vh"
-        borderRadius="lg"
-        justifyContent="space-evenly"
         flexDir="row"
+        alignItems="center"
+        justifyContent="space-evenly"
+        backgroundColor="black"
+        m="7"
+        borderRadius="2xl"
+        borderColor="whiteAlpha.400"
+        border="2px"
       >
-        <Flex width="50%" flexDir="column" justifyContent="center" m="3">
-          <Heading size="2xl" color="whiteAlpha.800" mb="5">
-            Mike Miller
-          </Heading>
-          <Image
-            src={require('../../img/mike.jpg')}
-            alt="Mike's headshot"
-            objectFit="cover"
-            height="400px"
-            border="2px"
-            borderColor="blackAlpha.800"
-            borderRadius="2xl"
-          />
-        </Flex>
-        <Text
-          fontSize="2xl"
-          width="50%"
-          color="whiteAlpha.800"
-          mt="auto"
-          mb="auto"
-        >
+        <Image
+          src={require('../../img/mike.jpg')}
+          alt="Mike's headshot"
+          maxW="500px"
+          border="2px"
+          borderColor="blackAlpha.800"
+          borderRadius="2xl"
+          m="30px"
+        />
+        <Text fontSize="2xl" minWidth="300px" color="whiteAlpha.800" m="30px">
           Michael Miller is an aspiring full-stack web developer and musician in
           Woodbury, Minnesota. Miller just graduated from the University of
           Minnesota's Coding Bootcamp through Trilogy Education Services and is
           excited to find ways to utilize his new-found passion for coding.
         </Text>
       </Flex>
-    </section>
+    </Flex>
   );
 };
 

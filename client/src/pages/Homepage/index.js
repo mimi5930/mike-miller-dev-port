753e241a9655b96e0d15d8dc9eb3caca6dd6be9f
Mike <mikej.miller440@gmail.com>
2022-05-30 19:14:17 -0500

    started on landing page

--- client/src/pages/Homepage/index.js
+++ client/src/pages/Homepage/index.js
@@ -1,10 +1,13 @@
+import { Center, Heading } from '@chakra-ui/react';
 import React from 'react';
 
 const Homepage = props => {
   return (
-    <>
-      <></>
-    </>
+    <Center>
+      <Heading mt="25%" color="white" className="heading">
+        The computer is simply an instrument whose music is ideas
+      </Heading>
+    </Center>
   );
 };
 

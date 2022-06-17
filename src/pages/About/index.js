ec78d8c8b99a4f690aa9ac381625d9591c15199f
Mike <mikej.miller440@gmail.com>
2022-06-16 20:58:21 -0500

    adjusted styling

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -2,50 +2,49 @@ import { Heading, Text, Image, Flex } from '@chakra-ui/react';
 import React from 'react';
 
 const About = props => {
-  return (
-    <Flex
-      mt="20vh"
-      bgColor="blackAlpha.900"
-      width="80%"
-      mr="auto"
-      ml="auto"
-      height="60vh"
-      borderRadius="lg"
-      alignItems="center"
-      justifyContent="space-evenly"
-      flexDir="column"
-    >
-      <Heading size="4xl" color="whiteAlpha.800">
-        Mike Miller
-      </Heading>
-      <Flex
-        flexDir="row"
-        alignItems="center"
-        justifyContent="space-evenly"
-        backgroundColor="black"
-        m="7"
-        borderRadius="2xl"
-        borderColor="whiteAlpha.400"
-        border="2px"
-      >
-        <Image
-          src={require('../../img/mike.jpg')}
-          alt="Mike's headshot"
-          maxW="500px"
-          border="2px"
-          borderColor="blackAlpha.800"
-          borderRadius="2xl"
-          m="30px"
-        />
-        <Text fontSize="2xl" minWidth="300px" color="whiteAlpha.800" m="30px">
-          Michael Miller is an aspiring full-stack web developer and musician in
-          Woodbury, Minnesota. Miller just graduated from the University of
-          Minnesota's Coding Bootcamp through Trilogy Education Services and is
-          excited to find ways to utilize his new-found passion for coding.
-        </Text>
-      </Flex>
-    </Flex>
-  );
+	return (
+		<Flex
+			mt="250px"
+			width="80%"
+			mr="auto"
+			ml="auto"
+			height="300px"
+			borderRadius="lg"
+			alignItems="center"
+			justifyContent="space-evenly"
+			flexDir="column"
+		>
+			<Heading size="4xl" color="whiteAlpha.800">
+				Mike Miller
+			</Heading>
+			<Flex
+				flexDir="row"
+				alignItems="center"
+				justifyContent="space-evenly"
+				backgroundColor="black"
+				m="7"
+				borderRadius="2xl"
+				borderColor="whiteAlpha.400"
+				border="2px"
+			>
+				<Image
+					src={require('../../img/mike.jpg')}
+					alt="Mike's headshot"
+					maxW="500px"
+					border="2px"
+					borderColor="blackAlpha.800"
+					borderRadius="2xl"
+					m="30px"
+				/>
+				<Text fontSize="2xl" minWidth="300px" color="whiteAlpha.800" m="30px">
+					Michael Miller is an aspiring full-stack web developer and musician in
+					Woodbury, Minnesota. Miller just graduated from the University of
+					Minnesota's Coding Bootcamp through Trilogy Education Services and is
+					excited to find ways to utilize his new-found passion for coding.
+				</Text>
+			</Flex>
+		</Flex>
+	);
 };
 
 export default About;
--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -4,30 +4,30 @@ import { Center, Flex, Heading } from '@chakra-ui/react';
 import projects from './featProjects.json';
 
 const Portfolio = () => {
-  return (
-    <>
-      <Center>
-        <Heading color="white" m="5" mt="9rem">
-          Featured Work
-        </Heading>
-      </Center>
-      <Flex
-        flexDir="row"
-        flexWrap="wrap"
-        justifyContent="space-evenly"
-        bgColor="whiteAlpha.100"
-        m="5"
-        borderRadius="lg"
-        height="70%"
-        overflowY="scroll"
-        className="projects"
-      >
-        {projects.map(project => {
-          return <Project key={project.name} project={project} />;
-        })}
-      </Flex>
-    </>
-  );
+	return (
+		<>
+			<Center>
+				<Heading color="white" m="5" mt="9rem">
+					Featured Work
+				</Heading>
+			</Center>
+			<Flex
+				flexDir="row"
+				flexWrap="wrap"
+				justifyContent="space-evenly"
+				bgColor="whiteAlpha.100"
+				m="5"
+				borderRadius="lg"
+				height="65%"
+				overflowY="scroll"
+				className="projects"
+			>
+				{projects.map(project => {
+					return <Project key={project.name} project={project} />;
+				})}
+			</Flex>
+		</>
+	);
 };
 
 export default Portfolio;

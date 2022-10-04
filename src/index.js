fcfc972fb2d8d75beda82d60c63236c6b75c3f50
Mike <mikej.miller440@gmail.com>
2022-10-04 12:14:12 -0500

    commented out navbar

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ChakraProvider } from '@chakra-ui/react';
+// import { ChakraProvider } from '@chakra-ui/react';
 
-import theme from './utils/theme';
+// import theme from './utils/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    {/* <ChakraProvider theme={theme}> */}
+    <App />
+    {/* </ChakraProvider> */}
   </React.StrictMode>
 );

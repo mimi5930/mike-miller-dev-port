8c6c8bbb60b957761f5ed45e389f847f8a600fd8
Mike <mikej.miller440@gmail.com>
2022-10-04 12:13:56 -0500

    initial navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './navbar.css';
 
 // TODO: Add scroll hook to change navbar opacity
@@ -6,8 +6,29 @@ import './navbar.css';
 // TODO: Navigation Links Scroll to different sections
 
 export default function Navbar() {
+  function onScroll() {
+    console.log('scollY', window.scrollY);
+    if (window.scrollY <= 100) setScroll(window.scrollY);
+    else {
+      setScroll(0);
+    }
+  }
+  const [scroll, setScroll] = useState(window.scrollY);
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, [window.scrollY]);
+
   return (
-    <section className="nav-container">
+    <section
+      className="nav-container"
+      style={{
+        backgroundColor: `#253237${100 - window.scrollY}`
+      }}
+    >
       <div className="title-container">
         <h1>Michael Miller</h1>
         <h2>Web Developer</h2>

2a0ad466f5113dc42ad335ab2933b5781f6177a2
mimi5930 <mikej.miller440@gmail.com>
2022-10-10 10:00:58 -0500

    add portfolio page

---
+++ src/pages/Portfolio/Portfolio.jsx
@@ -0,0 +1,10 @@
+import './portfolio.css';
+import ProjectDescription from '../../components/ProjectDescription';
+
+export default function Portfolio() {
+  return (
+    <div>
+      <ProjectDescription></ProjectDescription>
+    </div>
+  );
+}

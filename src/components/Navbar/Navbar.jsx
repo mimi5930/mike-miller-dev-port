f292f9f89cfa4dde6edba3bc823e1dba0c2f7b23
Mike <mikej.miller440@gmail.com>
2022-10-12 18:49:12 -0500

    added navbar onScroll styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,34 +1,24 @@
 import { useEffect, useState } from 'react';
 import './navbar.css';
 
-// TODO: Add scroll hook to change navbar opacity
-// TODO: Add Icon?
 // TODO: Navigation Links Scroll to different sections
 
 export default function Navbar() {
   function onScroll() {
-    console.log('scollY', window.scrollY);
-    if (window.scrollY <= 100) setScroll(window.scrollY);
-    else {
-      setScroll(0);
-    }
+    window.scrollY ? setScroll(true) : setScroll(false);
   }
-  const [scroll, setScroll] = useState(window.scrollY);
+
+  const [scroll, setScroll] = useState(window.scrollY ? true : false);
 
   useEffect(() => {
     window.addEventListener('scroll', onScroll);
     return () => {
       window.removeEventListener('scroll', onScroll);
     };
-  }, [window.scrollY]);
+  }, []);
 
   return (
-    <section
-      className="nav-container"
-      style={{
-        backgroundColor: `#253237${100 - window.scrollY}`
-      }}
-    >
+    <section className={`nav-container ${scroll && 'nav-container-scrolled'}`}>
       <div className="title-container">
         <h1>Michael Miller</h1>
         <h2>Web Developer</h2>

f7c6d2ddeab529ecb544f5f420a5f7513c71e1b8
Mike Miller <mikej.miller440@gmail.com>
2024-09-11 16:49:48 -0500

    got rid of old stylings

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,4 @@
-// import * as React from 'react';
-// // import { ChakraProvider } from '@chakra-ui/react';
-// import * as ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-
-// const rootElement = document.getElementById('root') as HTMLElement;
-
-// ReactDOM.createRoot(rootElement).render(
-//   <React.StrictMode>
-//     {/* <ChakraProvider> */}
-//       <App />
-//     {/* </ChakraProvider> */}
-//   </React.StrictMode>
-// );
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App'
 
 const container = document.getElementById('root') as HTMLElement
